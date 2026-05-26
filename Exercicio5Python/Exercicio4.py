029554ca8710888c34525cacbd3934ec7ff16762
Leandro Ludwig Batista <ludwigleandro150@gmail.com>
2026-05-26 16:11:47 -0300

    Add files via upload

---
+++ Exercicio5Python/Exercicio4.py
@@ -0,0 +1,10 @@
+multiplicacao = 0 
+contador = 1
+
+
+while contador <=10:
+   numero = input("Digite o numero para fazer a tabuada: ")
+   multiplicacao *= int(numero)
+   contador += 1
+   print(multiplicacao)
+     
